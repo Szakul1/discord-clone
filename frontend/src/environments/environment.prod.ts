fcfceed604628b465310ea997e9cbf9b0dfdd607
Szakul1 <lukasz.szypski@gmail.com>
2022-12-01 19:23:48 +0100

    cd ci testing

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://backend-production-f7ce.up.railway.app',
-  url: 'https://frontend-production-47b5.up.railway.app'
+  url: 'https://frontend-production-ac53.up.railway.app'
 };
