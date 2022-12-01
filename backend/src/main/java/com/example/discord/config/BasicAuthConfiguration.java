4725f34f729e4e6df4fab0dc75fea2097cff4b0d
Szakul1 <lukasz.szypski@gmail.com>
2022-12-01 19:44:01 +0100

    edit config for local

--- backend/src/main/java/com/example/discord/config/BasicAuthConfiguration.java
+++ backend/src/main/java/com/example/discord/config/BasicAuthConfiguration.java
@@ -2,6 +2,7 @@ package com.example.discord.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
